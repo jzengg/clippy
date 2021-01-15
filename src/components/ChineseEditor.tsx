15b76a2f1814e8d3cdbf532fc26a6bae00855320
Jimmy Zeng <jzengg@gmail.com>
2021-01-14 23:24:58 -0500

    always save editor content and remove save button

--- src/components/ChineseEditor.tsx
+++ src/components/ChineseEditor.tsx
@@ -20,6 +20,16 @@ export default function ChineseEditor() {
         : EditorState.createEmpty();
     return defaultEditorState;
   });
+  const onChange = React.useCallback(
+    (newEditorState) => {
+      setEditorState(newEditorState);
+      const contentState = newEditorState.getCurrentContent();
+      const serializedContent = JSON.stringify(convertToRaw(contentState));
+      localStorage.setItem(SAVED_EDITOR_STATE_KEY, serializedContent);
+    },
+    [editorState, setEditorState]
+  );
+
   const [savedCharactersData, setSavedCharactersData] = React.useState<
     ChineseCharacterData[]
   >(() => {
@@ -37,20 +47,12 @@ export default function ChineseEditor() {
   const selectedText = getSelectionText(editorState)?.trim();
   const isCharacterSelected = selectedText != null && selectedText != "";
 
-  const saveText = () => {
-    localStorage.setItem(
-      SAVED_EDITOR_STATE_KEY,
-      JSON.stringify(convertToRaw(editorState.getCurrentContent()))
-    );
-  };
-
   function addSavedCharacter(ChineseCharacterData: ChineseCharacterData) {
     const newState = [...savedCharactersData, { ...ChineseCharacterData }];
     localStorage.setItem(SAVED_CHARACTERS_DATA_KEY, JSON.stringify(newState));
     setSavedCharactersData(newState);
     setExportData(null);
   }
-
   function removeSavedCharacter(indexToRemove: number) {
     setSavedCharactersData(
       savedCharactersData.filter((_char, idx) => idx !== indexToRemove)
@@ -72,9 +74,8 @@ export default function ChineseEditor() {
         <Editor
           placeholder="Paste Chinese text"
           editorState={editorState}
-          onChange={setEditorState}
+          onChange={onChange}
         />
-        <button onClick={saveText}>Save Text</button>
       </div>
       <div className="grid-col">
         {isCharacterSelected && (
