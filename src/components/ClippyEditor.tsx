9ba0357dce1cfffd1e8a1354235647ad98bf4a8a
Jimmy Zeng <jzengg@gmail.com>
2021-01-17 19:31:28 -0500

    add ctrl key as shortcut

--- src/components/ClippyEditor.tsx
+++ src/components/ClippyEditor.tsx
@@ -2,20 +2,23 @@ import React from "react";
 import { DraftHandleValue, Editor, EditorState } from "draft-js";
 import { ClippyCharacterData } from "../types/interfaces";
 import { getDefaultKeyBinding, KeyBindingUtil } from "draft-js";
-const { hasCommandModifier } = KeyBindingUtil;
+const { hasCommandModifier, isCtrlKeyCommand } = KeyBindingUtil;
 
 const SAVE_CHARACTER_COMMAND = "myeditor-save-character";
 const SELECT_DEFINITION_UP_COMMAND = "myeditor-select-definition-up";
 const SELECT_DEFINITION_DOWN_COMMAND = "myeditor-select-definition-down";
 
 function myKeyBindingFn(e: React.KeyboardEvent<{}>): string | null {
-  if ((e.key === "s" || e.key === "Enter") && hasCommandModifier(e)) {
+  if (
+    (e.key === "s" || e.key === "Enter") &&
+    (hasCommandModifier(e) || isCtrlKeyCommand(e))
+  ) {
     return SAVE_CHARACTER_COMMAND;
   }
-  if (e.key === "ArrowDown" && hasCommandModifier(e)) {
+  if (e.key === "ArrowDown" && (hasCommandModifier(e) || isCtrlKeyCommand(e))) {
     return SELECT_DEFINITION_DOWN_COMMAND;
   }
-  if (e.key === "ArrowUp" && hasCommandModifier(e)) {
+  if (e.key === "ArrowUp" && (hasCommandModifier(e) || isCtrlKeyCommand(e))) {
     return SELECT_DEFINITION_UP_COMMAND;
   }
   return getDefaultKeyBinding(e);
