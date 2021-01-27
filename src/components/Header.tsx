d386c6f5e28f46e41802ad0fbdfd2f5d5f846934
Jimmy Zeng <jzengg@gmail.com>
2021-01-26 19:36:09 -0500

    invalidate index.html and add version to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Heading } from "@chakra-ui/react";
+import packageJson from '../../package.json'
 
 export default function Header() {
   return (
     <div className="header">
-      <Heading p={2} size="lg" color="red.600" as="h1">
+      <Heading p={2} size="lg" color="red.600" as="h1" title={packageJson.version}>
         Clippy
       </Heading>
     </div>
